e49c110807fc4ff66f62e20859ae93571a04065a
ghkdus <100833948+ghkdus@users.noreply.github.com>
2022-05-25 16:21:36 +0900

    Update 11주차 팀 과제.py

--- 11주차 팀 과제.py
+++ 11주차 팀 과제.py
@@ -1,1 +1,18 @@
+#인터파크 베스트 아이템(순위, 제품명, 가격, 무료배송여부) 크롤러
+from urllib.request import urlopen
+
+url = 'https://shopping.interpark.com/best/main.do'
+
+htmlData = urlopen(url).read()
+
+htmlData=htmlData.decode('utf8')
+
+from bs4 import BeautifulSoup as bs
+
+html = bs(htmlData, 'html.parser')
+
+name = html.select('.name')
+num = html.select('.number')
+benefit = html.select('.benefit')
+rank = html.select('h3')
 
